35e3a87c85b05df8a8437c61406e276f31648fd9
muliang <muliang@baicizhan.com>
2021-01-04 10:31:56 +0800

    feat: fullfilled

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,1 @@
+console.log("keep for rails 5.0+")
--- config/initializers/init_model.rb
+++ config/initializers/init_model.rb
@@ -1,7 +1,6 @@
-
-db = ActiveRecord::Base.configurations[Rails.env]
-
-if db['auto']
+# frozen_string_literal: true
+if true & ENV['GM']
+  db = ActiveRecord::Base.configurations[Rails.env]
   cmd = "rmre -a mysql2 -d #{db['database']} -o app/models -u #{db['username']} -p #{db['password']}"
   p system(cmd)
 end
